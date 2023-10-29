37a9da84503f54c3bcbd80ae7a01915ce394e293
prames <134806363+0xprames@users.noreply.github.com>
2023-10-29 10:05:18 -0400

    (feat) add tcp listener to network for incoming connections, handle too-many-peers after this (#23)

--- bins/reth-crawler/src/crawler/service.rs
+++ bins/reth-crawler/src/crawler/service.rs
@@ -1,6 +1,7 @@
 use futures::join;
 use reth_discv4::Discv4;
 use reth_dns_discovery::DnsDiscoveryHandle;
+use reth_network::NetworkHandle;
 use reth_primitives::NodeRecord;
 use secp256k1::SecretKey;
 use tokio::sync::mpsc;
@@ -12,16 +13,22 @@ pub struct CrawlerService {
 }
 
 impl CrawlerService {
-    pub async fn new(discv4: Discv4, dnsdisc: DnsDiscoveryHandle, key: SecretKey) -> Self {
+    pub async fn new(
+        discv4: Discv4,
+        dnsdisc: DnsDiscoveryHandle,
+        network: NetworkHandle,
+        key: SecretKey,
+    ) -> Self {
         let (tx, rx) = mpsc::unbounded_channel::<Vec<NodeRecord>>();
-        let updates = UpdateListener::new(discv4.clone(), dnsdisc.clone(), key, tx.clone()).await;
+        let updates = UpdateListener::new(discv4, dnsdisc, network, key, tx).await;
         Self { updates }
     }
 
-    pub async fn run(self, save_to_json: bool) -> (eyre::Result<()>, eyre::Result<()>) {
+    pub async fn run(self, save_to_json: bool) -> (eyre::Result<()>, eyre::Result<()>, ()) {
         join!(
             self.updates.start_discv4(save_to_json),
             self.updates.start_dnsdisc(save_to_json),
+            self.updates.start_network(save_to_json),
         )
     }
 }
