ec7e43ead16c17effeebde302e3a6ef9c03f2863
0xprames <0xprames@proton.me>
2023-11-01 21:43:51 -0400

    (feat) add /clients endpoint for client diversity dashboard

--- bins/api-server/src/peerdb/routes.rs
+++ bins/api-server/src/peerdb/routes.rs
@@ -5,7 +5,7 @@ use axum::{
     routing::get,
     Json, Router,
 };
-use reth_crawler_db::{PeerDB, PeerData};
+use reth_crawler_db::{types::ClientData, PeerDB, PeerData};
 
 use super::app_state::AppState;
 
@@ -14,10 +14,26 @@ pub fn rest_router() -> Router<AppState> {
         .route("/nodes", get(get_nodes))
         .route("/node/id/:id", get(get_node_by_id))
         .route("/node/ip/:ip", get(get_node_by_ip))
+        .route("/clients", get(get_clients))
 }
 
 async fn get_nodes(State(store): State<Arc<dyn PeerDB>>) -> Json<Vec<PeerData>> {
     Json(store.all_peers(Some(50)).await.unwrap())
+}
+
+async fn get_clients(State(store): State<Arc<dyn PeerDB>>) -> Json<Vec<ClientData>> {
+    Json(
+        store
+            .all_peers(Some(50))
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|peer| {
+                let client_version = peer.client_version;
+                ClientData { client_version }
+            })
+            .collect(),
+    )
 }
 
 async fn get_node_by_id(
