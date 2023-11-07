c481da96bef2051dc8344b93f183025913447745
alessandromazza <alessandro98.mazza@gmail.com>
2023-11-07 18:25:05 +0100

    listen for new blocks and add an in-memory state to the crawler

--- bins/reth-crawler/src/crawler/service.rs
+++ bins/reth-crawler/src/crawler/service.rs
@@ -5,6 +5,7 @@ use reth_network::NetworkHandle;
 use reth_primitives::NodeRecord;
 use secp256k1::SecretKey;
 use tokio::sync::mpsc;
+use tracing::info;
 
 use crate::crawler::listener::UpdateListener;
 
@@ -27,11 +28,17 @@ impl CrawlerService {
         Self { updates }
     }
 
-    pub async fn run(self) -> (eyre::Result<()>, eyre::Result<()>, ()) {
+    pub async fn run(self) -> (eyre::Result<()>, eyre::Result<()>, (), eyre::Result<()>) {
+        // first initialize the state
+        info!("start initializing the state...");
+        let _ = self.updates.initialize_state().await;
+        // then start crawling
+        info!("start crawling...");
         join!(
             self.updates.start_discv4(),
             self.updates.start_dnsdisc(),
             self.updates.start_network(),
+            self.updates.start_state(),
         )
     }
 }
